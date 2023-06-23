d1db6a2b1a68015f0e8bb2d36a9876cb7b27d47f
SHUBAAA <m.alvial03@ufromail.cl>
2023-06-23 16:40:26 -0400

    Crear usuario

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import usuarioRoutes from './routes/usuarios.routes.js';
+import productoRoutes from './routes/producto.routes.js';
+import envs from './configs/environments.js';
+import connect from './configs/mongo.js';
+
+
+
+const app = express();
+
+
+app.use(express.json());
+app.use('', usuarioRoutes);
+
+
+console.log('Connecting to database...');
+console.log(envs.MONGO_URI);
+connect(envs.MONGO_URI)
+    .then(() => {
+        console.log('Mongo connected successful');
+        app.listen(3000, async () => {
+            console.log(`Server is running on PORT: 3000`);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+        process.exit(-1);
+    });
